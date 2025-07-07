802f3ed7e3629bb8f45d3b43a53bceffdac0a641
Khaled1771 <khhaledd.77@gmail.com>
2025-07-07 16:05:58 +0300

    i Think now 100% GitOps

--- GitOps.sh
+++ GitOps.sh
@@ -2,8 +2,12 @@
 
 IMAGE_TAG=$1
 # This shell script for update remote git repo
+git checkout feature/sensebox-fetch 
+git merge main
 git add Kubernetes/Flask-Deloyment.yaml
 git commit -m "Update HiveBox's imageTag: ${IMAGE_TAG}"
+git checkout main
+git merge feature/sensebox-fetch
 git push origin main
 
 # Sync HiveBox app with ArgoCD
