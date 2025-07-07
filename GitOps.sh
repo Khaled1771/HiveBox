0fb88e8eea055925152563e5232db996f996c6f1
Khaled1771 <khhaledd.77@gmail.com>
2025-07-07 16:58:59 +0300

    Fixing Github permission in Jenkins

--- GitOps.sh
+++ GitOps.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
+echo "######### Configuring Git Repo #########"
 set +x  # Hide sensitive output
 IMAGE_TAG=$1
 # GIT_USERNAME=${GIT_USER}
 # GIT_PASSWORD=${GIT_PASS}
 REPO_URL="https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Khaled1771/HiveBox.git"
 # Configure Git
+cd /mnt/MyData/Courses/Projects/HiveBox
 git config user.name "Khaled1771"
 git config user.email "khhaledd.77@gmail.com"
 git remote set-url origin $REPO_URL
+set -x  # Show logs in pipeline
+echo "######### Git Operations #########"
 # This shell script for update remote git repo
 git checkout main
 git pull origin main
