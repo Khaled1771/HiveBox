f938db27d5e8cf138a81f001da04b3fbe361e77b
Khaled1771 <khhaledd.77@gmail.com>
2025-07-12 18:36:07 +0300

    EKS Resource with IAM Role

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,31 +13,31 @@ resource "aws_subnet" "subnet1" {
   map_public_ip_on_launch = true
 }
 
-# resource "aws_eks_cluster" "k8s" {
-#   name     = "hivebox-eks"
-#   role_arn = aws_iam_role.eks_cluster_role.arn
+resource "aws_eks_cluster" "k8s" {
+  name     = "hivebox-eks"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
-#   vpc_config {
-#     subnet_ids = [aws_subnet.subnet1.id]
-#   }
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet1.id]
+  }
 
-#   depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
-# }
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
+}
 
-# resource "aws_iam_role" "eks_cluster_role" {
-#   name = "eksClusterRole"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eksClusterRole"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = "sts:AssumeRole",
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "eks.amazonaws.com"
-#       }
-#     }]
-#   })
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
 
 # resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
 #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
