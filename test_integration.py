8997c9cf34a7ecd1a0b203a7d8813977d883ed20
Khaled1771 <khhaledd.77@gmail.com>
2025-08-02 16:53:11 +0300

    Apply all types of testing on test namespace

--- test_integration.py
+++ test_integration.py
@@ -9,8 +9,8 @@ def test_temperature_endpoint_integration():
     
     Test on Kubernetes testing namespace
     """
-    ip = os.environ.get("HIVEBOX_IP", "hivebox-service.default.svc.cluster.local")
-    url = f"http://hivebox-service.default.svc.cluster.local:5000/temperature"       # Kubernetes Service insted of ingress
+    ip = os.environ.get("HIVEBOX_IP", "hivebox-service.testing.svc.cluster.local")
+    url = f"http://hivebox-service.testing.svc.cluster.local:5000/temperature"       # Kubernetes Service insted of ingress
 
     response = requests.get(url)
     assert response.status_code in [200, 404]
